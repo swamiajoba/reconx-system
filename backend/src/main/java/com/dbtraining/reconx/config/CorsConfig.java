8ba4cfee11dcc54d02b4853bcb42be712e62957b
manju <manju@abc.com>
2026-07-07 23:17:20 +0530

    modified Cors Config

--- backend/src/main/java/com/dbtraining/reconx/config/CorsConfig.java
+++ backend/src/main/java/com/dbtraining/reconx/config/CorsConfig.java
@@ -3,8 +3,8 @@ package com.bank.trading.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;   //
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 public class CorsConfig {
@@ -19,5 +19,4 @@ public class CorsConfig {
         source.registerCorsConfiguration("/**", config);
         return source;
     }
-
 }
